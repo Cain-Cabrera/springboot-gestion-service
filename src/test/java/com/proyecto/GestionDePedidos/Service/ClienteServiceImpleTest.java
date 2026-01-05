d9367f4cabd46b88073ff91a4460e5fb7028de3f
Cain-Cabrera <caincito.cabrera7@gmail.com>
2026-01-05 00:25:23 -0300

    se agrego happypath y errorpath de el metodo updateCliente

--- src/test/java/com/proyecto/GestionDePedidos/Service/ClienteServiceImpleTest.java
+++ src/test/java/com/proyecto/GestionDePedidos/Service/ClienteServiceImpleTest.java
@@ -2,6 +2,7 @@ package com.proyecto.GestionDePedidos.Service;
 
 import com.proyecto.GestionDePedidos.DTO.ClienteRequestDTO;
 import com.proyecto.GestionDePedidos.DTO.ClienteResponseDTO;
+import com.proyecto.GestionDePedidos.Exception.ClienteNotFoundException;
 import com.proyecto.GestionDePedidos.Mapper.ClienteMapper;
 import com.proyecto.GestionDePedidos.Repository.ClienteRepository;
 import com.proyecto.GestionDePedidos.models.Cliente;
@@ -9,6 +10,7 @@ import com.proyecto.GestionDePedidos.testdata.ClienteRequestTestBuilder;
 import com.proyecto.GestionDePedidos.testdata.ClienteResponseTestBuilder;
 import com.proyecto.GestionDePedidos.testdata.ClienteTestBuilder;
 import com.proyecto.GestionDePedidos.validatorService.ClienteValidator;
+import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -91,5 +93,60 @@ public class ClienteServiceImpleTest {
         assertThrows(IllegalArgumentException.class, () -> {
             clienteServiceImple.createCliente(clienteRequest);
         }); 
+    }
+    
+    @Test
+    void updateCliente_clienteActualizado_siEsValidoIdyCliente() {
+        Cliente cliente = ClienteTestBuilder.unCliente()
+                .conId(1L)
+                .conNombre("Cain")
+                .conApellido("Cabrera")
+                .conDni("440206015")
+                .conPedidos(null)
+                .build();
+        
+        ClienteRequestDTO clienteRequest = ClienteRequestTestBuilder.unClienteRequest()
+                .conNombre("Oscar")
+                .conApellido("Lopez")
+                .conDni("44206015")
+                .build();
+        
+        ClienteResponseDTO clienteResponse = ClienteResponseTestBuilder.unClienteResponse()
+                .conNombre("Oscar")
+                .conApellido("Lopez")
+                .conId(1L)
+                .build();
+        
+        
+        when(clienteRepository.save(cliente))
+                .thenReturn(cliente);
+        when(clienteMapper.toResponse(cliente))
+                .thenReturn(clienteResponse);
+        when(clienteRepository.findById(1L))
+                .thenReturn(Optional.of(cliente));
+        
+        ClienteResponseDTO respuesta = clienteServiceImple.updateCliente(1L, clienteRequest);
+        
+        assertNotNull(respuesta);
+        assertEquals(1L, respuesta.getId());
+        assertEquals("Oscar",respuesta.getNombre());
+        assertEquals("Lopez",respuesta.getApellido());
+    } 
+    
+    @Test
+    void updateCliente_clienteNoActualizado_siEsInvalidoId() {
+        ClienteRequestDTO clienteRequest = ClienteRequestTestBuilder.unClienteRequest()
+                .conApellido("")
+                .conDni("44206015")
+                .build();
+        
+        when(clienteRepository.findById(-1L))
+            .thenReturn(Optional.empty());
+         
+        
+        assertThrows(ClienteNotFoundException.class, () -> {
+            clienteServiceImple.updateCliente(-1L,clienteRequest);
+        }); 
+                
     }
 }
